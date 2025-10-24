bc107dc3adba1f4acce93f495dfafe44b371fc9a
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2025-10-24 10:52:53 -0400

    Make eyeball class

---
+++ eyeball-with-class/Eyeball.cs
@@ -0,0 +1,51 @@
+﻿using MohawkGame2D;
+using System;
+using System.Numerics;
+
+public class Eyeball
+{
+    Color irisColor = new Color(128, 64, 0);
+    Vector2 position = new Vector2(200, 200);
+    float corneaR = 50;
+    float irisR = 30;
+    float pupilR = 15;
+
+    public void DrawEyeball()
+    {
+        // General draw config
+        Draw.LineColor = Color.Black;
+        Draw.LineSize = 1;
+
+        // Draw Cornea
+        Draw.FillColor = Color.White;
+        Draw.Circle(position, corneaR);
+
+        // Math out vector from eye to mouse
+        Vector2 mousePosition = Input.GetMousePosition();
+        Vector2 fromEyeToMouse = mousePosition - position;
+        float distanceToMouse = fromEyeToMouse.Length();
+        Vector2 directionToMouse = Vector2.Normalize(fromEyeToMouse);
+
+        // Calculate where iris an pupil will go
+        Vector2 irisPosition;
+        if (distanceToMouse < corneaR - irisR)
+        {
+            // mouse is on top of eye, draw at mouse position
+            irisPosition = mousePosition;
+        }
+        else
+        {
+            // mouse is outside of eye, draw eye towards mouse
+            irisPosition = position + directionToMouse * (corneaR - irisR);
+        }
+
+        // Draw Iris
+        Draw.FillColor = irisColor;
+        Draw.Circle(irisPosition, irisR);
+
+        // Draw Pupil
+        Draw.FillColor = Color.Black;
+        Draw.Circle(irisPosition, pupilR);
+    }
+}
+
--- eyeball-with-class/Game.cs
+++ eyeball-with-class/Game.cs
@@ -7,11 +7,7 @@ namespace MohawkGame2D
 {
     public class Game
     {
-        Color irisColor = new Color(128, 64, 0);
-        Vector2 position = new Vector2(200, 200);
-        float corneaR = 50;
-        float irisR = 30;
-        float pupilR = 15;
+        Eyeball eyeball = new Eyeball();
 
         public void Setup()
         {
@@ -22,45 +18,7 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(Color.OffWhite);
-            DrawEyeball();
-        }
-
-        void DrawEyeball()
-        {
-            // General draw config
-            Draw.LineColor = Color.Black;
-            Draw.LineSize = 1;
-            
-            // Draw Cornea
-            Draw.FillColor = Color.White;
-            Draw.Circle(position, corneaR);
-
-            // Math out vector from eye to mouse
-            Vector2 mousePosition = Input.GetMousePosition();
-            Vector2 fromEyeToMouse = mousePosition - position;
-            float distanceToMouse = fromEyeToMouse.Length();
-            Vector2 directionToMouse = Vector2.Normalize(fromEyeToMouse);
-
-            // Calculate where iris an pupil will go
-            Vector2 irisPosition;
-            if (distanceToMouse < corneaR - irisR)
-            {
-                // mouse is on top of eye, draw at mouse position
-                irisPosition = mousePosition;
-            }
-            else
-            {
-                // mouse is outside of eye, draw eye towards mouse
-                irisPosition = position + directionToMouse * (corneaR - irisR);
-            }
-
-            // Draw Iris
-            Draw.FillColor = irisColor;
-            Draw.Circle(irisPosition, irisR);
-
-            // Draw Pupil
-            Draw.FillColor = Color.Black;
-            Draw.Circle(irisPosition, pupilR);
+            eyeball.DrawEyeball();
         }
     }
 
