25514ede3812a0f2fefc048bfd27ad10d8e7148c
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2025-10-24 10:47:18 -0400

    Add project

---
+++ eyeball-with-class/Game.cs
@@ -0,0 +1,67 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    public class Game
+    {
+        Color irisColor = new Color(128, 64, 0);
+        Vector2 position = new Vector2(200, 200);
+        float corneaR = 50;
+        float irisR = 30;
+        float pupilR = 15;
+
+        public void Setup()
+        {
+            Window.SetTitle("Eyeball with Class");
+            Window.SetSize(400, 400);
+        }
+
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+            DrawEyeball();
+        }
+
+        void DrawEyeball()
+        {
+            // General draw config
+            Draw.LineColor = Color.Black;
+            Draw.LineSize = 1;
+            
+            // Draw Cornea
+            Draw.FillColor = Color.White;
+            Draw.Circle(position, corneaR);
+
+            // Math out vector from eye to mouse
+            Vector2 mousePosition = Input.GetMousePosition();
+            Vector2 fromEyeToMouse = mousePosition - position;
+            float distanceToMouse = fromEyeToMouse.Length();
+            Vector2 directionToMouse = Vector2.Normalize(fromEyeToMouse);
+
+            // Calculate where iris an pupil will go
+            Vector2 irisPosition;
+            if (distanceToMouse < corneaR - irisR)
+            {
+                // mouse is on top of eye, draw at mouse position
+                irisPosition = mousePosition;
+            }
+            else
+            {
+                // mouse is outside of eye, draw eye towards mouse
+                irisPosition = position + directionToMouse * (corneaR - irisR);
+            }
+
+            // Draw Iris
+            Draw.FillColor = irisColor;
+            Draw.Circle(irisPosition, irisR);
+
+            // Draw Pupil
+            Draw.FillColor = Color.Black;
+            Draw.Circle(irisPosition, pupilR);
+        }
+    }
+
+}
